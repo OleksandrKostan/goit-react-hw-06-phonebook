f5026ca692d5ceef0886c2805db6464c93517277
Oleksandr Kostan <kostan.law@gmail.com>
2023-02-27 16:39:35 +0200

    redux_init

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,26 @@
 import useLocalStorage from 'components/Hooks/useLocalStorage';
 
-import  { useState } from 'react';
+
+
+import { useState } from 'react';
 
 import { ThemeProvider } from 'styled-components';
 import { theme } from './Theme';
 import { GlobalStyle } from './GlobalStyle';
 import { nanoid } from 'nanoid';
 
-import  ContactForm  from 'components/ContactForm/ContactForm';
+import ContactForm from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
 
-export  default function App () {
-   const [contacts, setContacts] = useLocalStorage('contacts', []);
+export default function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
   const [filter, setFilter] = useState('');
-  
-  
- 
 
- const addContact = ({ name, number }) => {
+
+
+
+  const addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
@@ -40,7 +42,7 @@ export  default function App () {
     setFilter(e.currentTarget.value);
   };
 
- const getFilteredContacts = () => {
+  const getFilteredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter),
@@ -48,37 +50,19 @@ export  default function App () {
   };
 
   const deleteContacts = contactId => {
-   setContacts(contacts.filter(contact => contact.id !== contactId),
+    setContacts(contacts.filter(contact => contact.id !== contactId),
     );
   };
 
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   const nextContacts = this.state.contacts;
-  //   const prevContacts = prevState.contacts;
-  //   if (nextContacts !== prevContacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(nextContacts));
-  //   }
-  // }
-  // // порівнюємо попередній стейт із наступним, та записуємо в локал сторедж
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts');
-  //   const parsedContacts = JSON.parse(contacts);
-  //   if (parsedContacts) {
-  //     this.setState({ contacts: parsedContacts });
-  //   }
-  // }
-  // витягуємо із локал сторедж, розпаршуємо та рендимо збережене
+  return (<> <ThemeProvider theme={theme}> <GlobalStyle />
+    <h1>Phonebook</h1>
+    <ContactForm onSubmit={addContact} />
+    <h2>Contacts</h2>
+    <Filter value={filter} onChange={changeFilter} />
+    <ContactList contacts={getFilteredContacts()}
+      onClickDelete={deleteContacts} />
+  </ThemeProvider> </>
+  );
 
- 
-    return (<> <ThemeProvider theme={theme}> <GlobalStyle />
-      <h1>Phonebook</h1>
-      <ContactForm onSubmit={addContact} />
-      <h2>Contacts</h2>
-      <Filter value={filter} onChange={changeFilter} />
-      <ContactList contacts={getFilteredContacts()}
-        onClickDelete={deleteContacts} />
-    </ThemeProvider> </>
-    );
-  
 }
